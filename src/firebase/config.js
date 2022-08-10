62d134de1bf8da1c7e11d204226a422e56a3b50c
IslamTashiev <ueaten04@gmail.com>
2022-08-10 12:33:44 +0600

    added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDULIV7TuRPxeBQcj5cIgS9A8wesZd6CU",
+  authDomain: "vue-todo-aha.firebaseapp.com",
+  projectId: "vue-todo-aha",
+  storageBucket: "vue-todo-aha.appspot.com",
+  messagingSenderId: "531399129263",
+  appId: "1:531399129263:web:9d637bd90cf2fddb207c5a",
+};
+
+const app = initializeApp(firebaseConfig);
